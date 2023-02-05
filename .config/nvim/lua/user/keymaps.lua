a614170425091f558d39ff9b50f32d0c754b82fa
Marius Büscher <marius@gridscale.io>
2023-02-05 12:26:45 +0100

    Add proper keymaps for telescope

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -6,3 +6,10 @@ local opts = { silent = true }
 
 -- NvimTree
 keymap("n", "<A-1>", ":NvimTreeToggle<CR>", opts)
+
+-- Telescope
+keymap("n", "<leader>ff", ":Telescope find_files hidden=true no_ignore=true<CR>", opts)
+keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
+keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
+keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+
