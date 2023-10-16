a9a2f7a434a5e3e4c04f198670524c2b60295b76
Marius Buescher <marius@gridscale.io>
2023-10-16 11:50:15 +0200

    Fix nvim tree setup

--- .config/nvim/lua/user/nvim-tree.lua
+++ .config/nvim/lua/user/nvim-tree.lua
@@ -3,14 +3,7 @@ if not status_ok then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
-nvim_tree.setup {
+nvim_tree.setup({
   update_focused_file = {
     enable = true,
     update_cwd = true,
@@ -25,12 +18,5 @@ nvim_tree.setup {
   view = {
     width = 30,
     side = "left",
-    mappings = {
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
   },
-}
+})
