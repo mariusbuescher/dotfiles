f5f3fd496b9b17413776ec0db1e93946ea46b6a4
Marius Buescher <marius@gridscale.io>
2025-03-19 15:48:11 +0100

    Fix typo

--- .setup.d/bashrc.d.sh
+++ .setup.d/bashrc.d.sh
@@ -1,6 +1,6 @@
 #! /bin/env bash
 
->&2 echo "checking $HOME/.bashrd.d"
+>&2 echo "checking $HOME/.bashrc.d"
 if [ ! -h $HOME/.bashrc.d ]; then
     >&2 echo "linking $HOME/.bashrc.d"
     ln -s $(pwd)/.bashrc.d $HOME/.bashrc.d
