13d5f7c4fed76dc7f4ce654274e5370ed8a5d8fa
Marius Buescher <marius.buescher@gmx.de>
2025-08-02 17:30:56 +0200

    Add config for neovim treesitter

---
+++ .config/nvim/lua/user/treesitter.lua
@@ -0,0 +1,15 @@
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+	return
+end
+
+configs.setup({
+  ensure_installed = { "bash", "c", "javascript", "json", "typescript", "tsx", "lua", "python", "css", "rust", "java", "yaml", "markdown", "markdown_inline", "php", "phpdoc", "go", "cue" }, -- one of "all" or a list of languages
+	highlight = {
+		enable = true, -- false will disable the whole extension
+	},
+	autopairs = {
+		enable = true,
+	},
+	indent = { enable = true, disable = { "python", "css" } },
+})
